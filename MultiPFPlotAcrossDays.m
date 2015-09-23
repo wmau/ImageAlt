e0fe6c9a4b81d55e5dbcdcecef6375e75ca3c301
wmau <wmau@bu.edu>
2015-09-23 16:27:42 -0400

    Conglomerate plot
    Some progress on plotting neuron masks, 2D place fields, 1D place
    fields, and sorted linearized place fields in the same figure. Not
    complete.

--- MultiPFPlotAcrossDays.m
+++ MultiPFPlotAcrossDays.m
@@ -1,50 +1,151 @@
-function MultiPFPlotAcrossDays(regfilepath,basedate,dates)
+function MultiPFPlotAcrossDays(regfilepath,dates)
 %
 %
 %
 
-%% 
-    %
-    [sortedrates,lneuronid,rneuronid] = multi_LinearizedPFs(regfilepath,basedate,dates);
-    close all;
+%% Initialize. 
+    %Extract dates. 
+    basedate = dates{1};
+    regdates = dates(2:end);
+    numDates = length(dates);
     
-    currentdir = pwd;
-    altdir = fileparts(regfilepath); 
-    basepath = fullfile(altdir,basedate); 
-    numdates = length(dates); 
+    %Define and preallocate. 
     mazetype = 'tmaze'; 
     data = struct; 
     
-    X = cell(numdates+1,1); 
-    lefttrials = cell(numdates+1,1); 
-    righttrials = cell(numdates+1,1); 
+    %Sortedrates is a 2xD cell array containing the plots we want for
+    %column 4 in our subplot. 
+    [sortedrates,lneuronid,rneuronid] = multi_LinearizedPFs(regfilepath,basedate,regdates);
+    close all;
+
+    %Get file paths. 
+    currentdir = pwd;
+    altdir = fileparts(regfilepath);            %Directory for alternation.
+    paths = cell(numDates,1);                   
     
-%% Get neuron masks. 
-    cd(basepath); 
-    load('ProcOut.mat','FT','NeuronImage'); 
-    data(1).left.Neurons = NeuronImage(lneuronid(:,1));
-    data(1).left.IDlist = lneuronid(:,1); 
-    data(1).right.Neurons = NeuronImage(rneuronid(:,1)); 
-    data(1).right.IDlist = rneuronid(:,1); 
+    for thisDate = 1:numDates
+        %All file paths of interest. 
+        paths{thisDate} = fullfile(altdir,dates{thisDate});       
+    end
+
+    %Preallocate. 
+    X = cell(numDates,1);                       %Linearized trajectory for each date. 
+    lefttrials = cell(numDates,1);              %Left trial firing rates.   
+    righttrials = cell(numDates,1);             %Right trial firing rates. 
     
-%% Get place maps.
-    load('PlaceMaps.mat','TMap'); 
-    data(1).left.PFs = TMap(lneuronid(:,1));
-    data(1).right.PFS = TMap(rneuronid(:,1)); 
-    
-%% Get linearized place maps. 
-    try 
-        load('Pos_align.mat','x_adj_cm','y_adj_cm');
-        x = x_adj_cm; y = y_adj_cm; 
-    catch 
-        load('Pos.mat','xpos_interp','ypos_interp'); 
-        x = xpos_interp; y = ypos_interp; 
+%% Loop through recording sessions and extract data. 
+    for thisDate = 1:numDates
+        cd(paths{thisDate}); 
+        disp(['Processing ', dates{thisDate}, '...']); 
+        
+        %% Get neuron masks.
+        load('ProcOut.mat','FT','NeuronImage'); 
+        
+        %Vector to preserve order. 
+        existCell{1} = ~isnan(lneuronid(:,thisDate));
+        existCell{2} = ~isnan(rneuronid(:,thisDate));
+        
+        %Preallocate. 
+        data(thisDate).left.Neurons = cell(size(existCell{1}));
+        data(thisDate).right.Neurons = cell(size(existCell{2})); 
+        
+        %Extract neuron masks. 
+        data(thisDate).left.Neurons(existCell{1}) = ...
+            NeuronImage(lneuronid(existCell{1},thisDate))';
+        data(thisDate).left.IDlist = lneuronid(:,thisDate); 
+        data(thisDate).right.Neurons(existCell{2}) = ...
+            NeuronImage(rneuronid(existCell{2},thisDate))';
+        data(thisDate).right.IDlist = rneuronid(:,thisDate); 
+        
+        %% Get place maps. 
+        load('PlaceMaps.mat','TMap'); 
+        
+        %Preallocate.
+        data(thisDate).left.PFs = cell(size(existCell{1}));
+        data(thisDate).right.PFs = cell(size(existCell{2})); 
+        
+        %Assign. 
+        data(thisDate).left.PFs(existCell{1}) = TMap(lneuronid(existCell{1},thisDate))';
+        data(thisDate).right.PFs(existCell{2}) = TMap(rneuronid(existCell{2},thisDate))';
+        
+        %% Get linearized place fields. 
+        try 
+            load('Pos_align.mat','x_adj_cm','y_adj_cm');
+            x = x_adj_cm; y = y_adj_cm; 
+        catch 
+            load('Pos.mat','xpos_interp','ypos_interp'); 
+            x = xpos_interp; y = ypos_interp; 
+        end
+        
+        %Each of these will be sorted within the context of their own
+        %recording session. 
+        X{thisDate} = LinearizeTrajectory(x,y,mazetype); 
+        [lefttrials{thisDate},righttrials{thisDate},~] = LinearizedPFs(X{thisDate},FT);
     end
     
-    X{1} = LinearizeTrajectory(x,y,mazetype); 
-    [lefttrials{1},righttrials{1},~] = LinearizedPFs(X{1},FT);
+%% Plot stuff. 
+    keepgoing = 1;
+    thisNeuron = 1;
+    numGoodCells = max([length(existCell{1}) length(existCell{2})]); 
+    ind = 1; 
     
-    keyboard;
-   
+    while keepgoing
+        figure(50);
+        figure(51); 
+        
+        %Row index. 
+        rowInc = 0; 
+        
+        for thisDate = 1:numDates
+            %Left trials. 
+            figure(50); 
+            
+            %Neuron mask. 
+            subplot(numDates,4,ind+rowInc);
+            imagesc(data(thisDate).left.Neurons{thisNeuron}); 
+            
+            %Place field. 
+            subplot(numDates,4,ind+rowInc+1);
+            imagesc(data(thisDate).left.PFs{thisNeuron});
+            
+            %Linearized PF plot with highlighted neuron. 
+            goodneurons = any(~isnan(lefttrials{thisDate}.rate),2); 
+            subplot(numDates,4,ind+rowInc+2);
+            imagesc(lefttrials{thisDate}.rate(goodneurons,:)); 
+            
+            %Right trials. 
+            figure(51); 
+            
+            %Neuron mask. 
+            subplot(numDates,4,ind+rowInc);
+            imagesc(data(thisDate).right.Neurons{thisNeuron}); 
+            
+            %Place field. 
+            subplot(numDates,4,ind+rowInc+1);
+            imagesc(data(thisDate).right.PFs{thisNeuron});
+            
+            %Linearized PF plot with highlighted neuron. 
+            goodneurons = any(~isnan(righttrials{thisDate}.rate),2); 
+            subplot(numDates,4,ind+rowInc+2);
+            imagesc(righttrials{thisDate}.rate(goodneurons,:));  
+            
+            rowInc = rowInc + 4; 
+        end
+        
+        figure(50); colormap('gray');
+        figure(51); colormap('gray');
+        
+        [~,~,key] = ginput(1); 
+        
+            if key == 29 && thisNeuron < numGoodCells
+                thisNeuron = thisNeuron + 1; 
+            elseif key == 28 && thisNeuron ~= 1
+                thisNeuron = thisNeuron - 1; 
+            elseif key == 27
+                keepgoing = 0; 
+                close all;  
+            end
+    end
+    
     cd(currentdir); 
 end
