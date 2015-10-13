4ada5bce6fcb1bf774ea3dfb8fe66f27e259c5b6
nkinsky <kinsky@bu.edu>
2015-10-13 12:54:06 -0400

    plot_type = 8 -> L and R trials color coded consistently in all subplots pretty hacky at this point

--- plotSigSplitters.m
+++ plotSigSplitters.m
@@ -8,7 +8,7 @@ function plotSigSplitters(session,plot_type,invert_raster_color)
 %      plot_type =  1 includes the significance curves at the bottomr, 2
 %           stacks all 3 in a single column, 3 produces only rasters. 4 is the same
 %           as 1 but tacks on the 3D Tmap at the end, and requires inputting the
-%           appropriate TMap cell after 4 (e.g. ...'plot_type,'4,TMap). 1 =
+%           appropriate TMap cell after 4 (e.g. ...'plot_type,4,TMap). 1 =
 %           default.
 %
 %   invert_raster_color: plots with background white and transients
@@ -29,24 +29,34 @@ end
     load('splittersByTrialType.mat','cellRespsByTrialType'); 
     load('sigSplitters.mat'); 
     
-    if plot_type == 4
+    if plot_type == 4 || plot_type == 7
        load('PlaceMaps.mat','TMap','OccMap')
        for j = 1:length(TMap)
            [OccMap_smooth, TMap_nan{j}] = make_nan_TMap(OccMap, TMap{j});
        end
+    elseif plot_type == 8
+        load('Tmaps_LR.mat')
+        for j = 1:length(Tleft)
+            [~, Tleft_nan{j}] = make_nan_TMap(RunOccMap, Tleft{j});
+            [~, Tright_nan{j}] = make_nan_TMap(RunOccMap, Tright{j});
+        end
     end
 
 %%  Get statistically significant splitters
     sigSplitters = find(cellfun(@any,sigcurve)); 
     savepdf = 0;            %Toggle whether you want to save all rasters as pdfs. 
     
-    if plot_type ~= 4
+    if plot_type ~= 4 && plot_type ~= 7 && plot_type ~= 8
         plotSplitters(cellRespsByTrialType,tuningcurves,deltacurve,sigcurve,...
             sigSplitters,savepdf,'plot_type',plot_type,'invert_raster_color',...
             invert_raster_color);
-    else
-       plotSplitters(cellRespsByTrialType,tuningcurves,deltacurve,sigcurve,...
+    elseif plot_type == 4 || plot_type == 7
+        plotSplitters(cellRespsByTrialType,tuningcurves,deltacurve,sigcurve,...
             sigSplitters,savepdf,'plot_type',plot_type,TMap_nan,'invert_raster_color',...
-            invert_raster_color); 
+            invert_raster_color);
+    elseif plot_type == 8
+        plotSplitters(cellRespsByTrialType,tuningcurves,deltacurve,sigcurve,...
+            sigSplitters,savepdf,'plot_type',plot_type, Tleft_nan, Tright_nan,'invert_raster_color',...
+            invert_raster_color);
     end
 end
--- plotSplitters.m
+++ plotSplitters.m
@@ -36,16 +36,23 @@ function plotSplitters(splittersByTrialType,tuningcurves,deltacurve,sigcurve,ind
 %               3 produces only rasters. 
 %               4 is the same as 1 but tacks on the 3D Tmap at the end, and
 %                   requires inputting the appropriate TMap cell after 4
-%                   (e.g. ...'plot_type,'4,TMap).
+%                   (e.g. ...'plot_type',4,TMap).
 %               5 concatenates the left and right turn rasters then
 %                   overlays a smoothed calcium event histogram. Note that
 %                   significance markers are not plotted because they're hard
 %                   to see. 
 %               6 does the same as 5 but the histogram is a separate
 %                   subplot.
+%               7 same as 6 but with TMap also, need to include TMap
+%                   variable as in 4.
+%               8 same as 6 but plots a difference of TMaps for left and
+%               right trials.  Must include Tmap_left and Tmap_right as 
+%                   inputs after 'plot_type'.
 %               
 %           invert_raster_color: plots with background white and transients
-%           in gray/black.  Default = 0 uses colormap(gray).
+%           in gray/black.  Default = 0 uses colormap(gray). 2 in
+%           conjunction with plot_type = 7 results in plotting left trials
+%           blue and right trials red
 %
 %           
 
@@ -71,8 +78,11 @@ invert_raster_color = 0; % default
 for j = 1:length(varargin)
    if strcmpi('plot_type',varargin{j})
        plot_type = varargin{j+1};
-       if plot_type == 4
+       if plot_type == 4 || plot_type == 7
            TMap_use = varargin{j+2};
+       elseif plot_type == 8
+           Tleft_nan = varargin{j+2};
+           Tright_nan = varargin{j+3};
        end
    end
    if strcmpi('invert_raster_color',varargin{j})
@@ -193,7 +203,9 @@ end
         %Concatenate the rasters. Now we have one matrix with left trials
         %first and right trials start at index numLTrials+1. 
         raster = [  splittersByTrialType{inds(i),1};
-                    splittersByTrialType{inds(i),2}     ];  
+                    -splittersByTrialType{inds(i),2}     ];  
+        n_left = size(splittersByTrialType{inds(i),1},1);
+        n_right = size(splittersByTrialType{inds(i),2},1);
                 
         %Smoothing.
         leftFit = fit([1:nBins]',tuningcurves{inds(i)}(1,:)','smoothingspline');
@@ -256,6 +268,95 @@ end
                 hold off;
                 set(gca,'ticklength',[0 0]); 
                 legend({'Left','Right'},'location','best');
+        elseif plot_type == 7 || plot_type == 8
+            subplot(3,1,1);           
+            imagesc(raster);
+            line(binDims,[nLTrials+0.5,nLTrials+0.5],'Color','g','LineWidth',2);  
+                set(gca,'ytick',[1:10:nTrials]);    
+                cmap = colormap('gray');
+                if invert_raster_color == 1
+                    cmap = flip(cmap,1);
+                    colormap(gca,cmap);
+                elseif invert_raster_color == 2
+                    cmap_use = [1 0 0; 1 1 1; 0 0 1];
+%                     cmap_use = [1 0 0; 0.5 0 0; 1 1 1; 1 1 1; 1 1 1; 0 0 0.5; 0 0 1];
+                    colormap(gca,cmap_use);
+                    caxis([-2 2]);
+                end
+                ylabel('Laps'); set(gca,'ticklength',[0 0]);
+                set(gca,'YTick',[1, n_left, n_left + n_right],...
+                    'YTickLabel',{num2str(1), num2str(n_left), num2str(n_left + n_right)})
+            
+            subplot(3,1,2);
+            plot(bins,leftCurve,'b',bins,rightCurve,'r','LineWidth',2);
+                xlabel('Stem Bins'); ylabel('RTransient Probability'); 
+                hold on;
+                
+            splitCurves = [leftCurve,rightCurve];
+            [BIN,SIG] = significance(deltacurve,sigcurve,splitCurves,inds,i,bins,1);
+            plot(BIN{1},SIG{1},'b*',BIN{2},SIG{2},'r*');
+                yLims = get(gca,'ylim');
+                ylim([0,yLims(2)]); xlim([1,nBins]);
+                hold off;
+                set(gca,'ticklength',[0 0]); 
+                legend({'Left','Right'},'location','best');
+                
+                if plot_type == 7
+                    h7 = subplot(3,1,3);
+                    imagesc_nan(rot90(TMap_use{inds(i)},-1));
+                    colormap(h7,'parula')
+                elseif plot_type == 8 % Plot difference between TMaps with left = blue and right = red
+                    h8 = subplot(3,1,3);
+                    % Hack to scale plots appropriately
+                    Tleft_use = Tleft_nan{inds(i)}*max(leftCurve)/max(Tleft_nan{inds(i)}(:));
+                    Tright_use = Tright_nan{inds(i)}*max(rightCurve)/max(Tright_nan{inds(i)}(:));
+                    imagesc_nan(rot90(Tleft_use-Tright_use,-1))
+                    cmap_use = [1 0 0 ; 0.875 0 0; 0.75 0 0 ; 0.625 0 0; 0.5 0 0 ; 0.375 0 0; 0.25 0 0 ; 0.125 0 0; ...
+                        0.9 0.9 0.9; 0.9 0.9 0.9; 0.9 0.9 0.9; 
+                        0 0 0.125; 0 0 0.25 ; 0 0 0.375; 0 0 0.5; 0 0 0.625; 0 0 0.75; 0 0 0.875; 0 0 1];
+                    colormap(h8,cmap_use)
+                end
+            
+        elseif plot_type == 9 % Hacked together from Jon's code - not tested yet
+            
+            %% Plot rasters
+            cmap = [0 0 1; 1 0 0];
+            h8 = subplot(3,1,1);
+            xlim_use = [0 size(raster,2)];
+            n_left_trials = size(splittersByTrialType{inds(i),1},1);
+            n_right_trials = size(splittersByTrialType{inds(i),2},1);
+            n_trials = size(raster,1);
+%             nn = 1;
+            for j = 1:n_trials
+                curspktrl = raster(j,:);
+                if j <= n_left_trials
+                   cmap_use = cmap(1,:);
+                elseif j > n_left_trials 
+                    cmap_use = cmap(2,:);
+                end
+%                 nn = nn + 1;
+%             curtrials=find(trialtype==c);
+                X=find( curspktrl > 0); %curspkpos(ismember(curspktrl,curtrials));
+                X=repmat(X(:)',3,1);
+                X=X(:);
+                Y=curspktrl(curspktrl > 0);
+                Y=[j+Y(:)'; Y(:)'+j+1; nan(1,numel(Y))];
+                Y=Y(:);
+                line(X,Y,'Parent',h8,'Color',cmap_use);
+                set(h8,'Xlim',[xlim_use(1) xlim_use(2)],...
+                    'Ylim',[1 n_trials+1],'Ydir','Reverse')
+                
+                h8_1 = subplot(3,1,3);
+                imagesc_nan(rot90(TMap_use{inds(i)},-1));
+                colormap(h8_1,'parula')
+            end
+            
+            % Plot significance curves
+            subplot(3,1,2);
+            plot(bins,leftCurve,'b',bins,rightCurve,'r','LineWidth',2);
+                xlabel('Stem Bins'); ylabel('Rate'); 
+                hold on;
+                
         end
             
 
